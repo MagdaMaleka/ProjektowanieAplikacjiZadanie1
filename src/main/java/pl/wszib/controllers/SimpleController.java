5d91755cc706c9e5a27c03fc2092341d493fd7dc
Magdalena Joanna Małek <mamaleka@student.wszib.edu.pl>
2019-04-06 15:50:44 +0200

    Stwórz nowy projekt Maven wykorzystaj zależności:
        <parent>
            <groupId>org.springframework.boot</groupId>
            <artifactId>spring-boot-starter-parent</artifactId>
            <version>2.1.3.RELEASE</version>
        </parent>
    
        <dependencies>
            <dependency>
                <groupId>org.springframework.boot</groupId>
                <artifactId>spring-boot-starter-web</artifactId>
                <version>2.1.3.RELEASE</version>
            </dependency>
            <dependency>
                <groupId>org.springframework.boot</groupId>
                <artifactId>spring-boot-starter-thymeleaf</artifactId>
                <version>2.1.3.RELEASE</version>
            </dependency>
            <dependency>
                <groupId>org.springframework.boot</groupId>
                <artifactId>spring-boot-devtools</artifactId>
                <optional>true</optional>
            </dependency>
        </dependencies>
    
    - Stwórz klasę
    Samochód - właściwości klasy:
        marka
        model
        rocznik
        cena
        przebieg w kilometrach
    
    - Stwórz kontroler z metodą (np. requestMapping: car/list GET) która będzie zwracać listę/tablicę samochodów (zmyślone dane) zawierającą około 10 elementów.
    
    - Stwórz widok dla kontrolera (car/list GET) wyświetlający listę przekazaną przez kontroler listy samochodów.
      Wyświetlaj tylko index (numer elementu na liście) markę oraz model
    
    - Stwórz metodę kontrolera (car/single/{index} GET) zwracająca pojedyńczy samochód na podstawie przekazanego indexu. Dodaj widok wyświetlający
      pojedyńczy samochód (wyświetlaj wszystkie dane)
    
    - Dodaj do widoku z listą samochodów link zawierający index, aby wykorzystać metodę kontrolera (car/single/{index} GET) zwracająca pojedyńczy samochód

---
+++ src/main/java/pl/wszib/controllers/SimpleController.java
@@ -0,0 +1,115 @@
+package pl.wszib.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.wszib.domain.Samochod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SimpleController {
+
+    @GetMapping("car")
+    public String car(Model model){
+        List<Samochod> list = generateList();
+        model.addAttribute("list", list);
+        return "carTemplate";
+    }
+
+    @RequestMapping(value="/single/{index}", method = RequestMethod.GET)
+    public String sinlge (Model model, @PathVariable("index") int index){
+        List<Samochod> list = generateList();
+        model.addAttribute("list", list.get(index));
+        return "singleTemplate";
+    }
+    private List<Samochod> generateList() {
+        List<Samochod> list = new ArrayList<>();
+
+        Samochod samochod1 = new Samochod();
+        samochod1.setMarka("Audi");
+        samochod1.setModel("A4");
+        samochod1.setRocznik(2000);
+        samochod1.setCena(15600);
+        samochod1.setPrzebieg(100000);
+        list.add(samochod1);
+
+        Samochod samochod2 = new Samochod();
+        samochod2.setMarka("Audi");
+        samochod2.setModel("A6");
+        samochod2.setRocznik(2013);
+        samochod2.setCena(35000);
+        samochod2.setPrzebieg(260000);
+        list.add(samochod2);
+
+        Samochod samochod3 = new Samochod();
+        samochod3.setMarka("Toyota");
+        samochod3.setModel("Avensis");
+        samochod3.setRocznik(2018);
+        samochod3.setCena(150000);
+        samochod3.setPrzebieg(10000);
+        list.add(samochod3);
+
+        Samochod samochod4 = new Samochod();
+        samochod4.setMarka("VW");
+        samochod4.setModel("Golf IV");
+        samochod4.setRocznik(2015);
+        samochod4.setCena(45000);
+        samochod4.setPrzebieg(50000);
+        list.add(samochod4);
+
+        Samochod samochod5 = new Samochod();
+        samochod5.setMarka("VW");
+        samochod5.setModel("Golf V");
+        samochod5.setRocznik(2013);
+        samochod5.setCena(60000);
+        samochod5.setPrzebieg(10000);
+        list.add(samochod5);
+
+        Samochod samochod6 = new Samochod();
+        samochod6.setMarka("VW");
+        samochod6.setModel("Polo");
+        samochod6.setRocznik(2014);
+        samochod6.setCena(35000);
+        samochod6.setPrzebieg(100000);
+        list.add(samochod6);
+
+        Samochod samochod7 = new Samochod();
+        samochod7.setMarka("Suzuki");
+        samochod7.setModel("Swift");
+        samochod7.setRocznik(2000);
+        samochod7.setCena(15000);
+        samochod7.setPrzebieg(200000);
+        list.add(samochod7);
+
+        Samochod samochod8 = new Samochod();
+        samochod8.setMarka("Seat");
+        samochod8.setModel("Toledo");
+        samochod8.setRocznik(2000);
+        samochod8.setCena(8000);
+        samochod8.setPrzebieg(260000);
+        list.add(samochod8);
+
+        Samochod samochod9 = new Samochod();
+        samochod9.setMarka("Seat");
+        samochod9.setModel("Altea");
+        samochod9.setRocznik(2017);
+        samochod9.setCena(15000);
+        samochod9.setPrzebieg(100000);
+        list.add(samochod9);
+
+        Samochod samochod10 = new Samochod();
+        samochod10.setMarka("Audi");
+        samochod10.setModel("A4");
+        samochod10.setRocznik(1999);
+        samochod10.setCena(6000);
+        samochod10.setPrzebieg(350000);
+        list.add(samochod10);
+
+        return list;
+    }
+}
